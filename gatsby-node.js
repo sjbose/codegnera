c613643a62b28a90cfa857c34529e66270e3dffc
Sj Bose <subhajeet.doc@gmail.com>
2021-04-12 20:11:53 +0530

    Deployment Fix2

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,43 @@
-const path = require("path")
+// const path = require("path")
 // const slash = require("slash");
 
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const articleTemplate = path.resolve(`src/templates/article.js`);
-  const response = await graphql(`
-    query {
-    allContentfulArticle{
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-    }
-  `)
-  //Create service pages
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   const articleTemplate = path.resolve(`src/templates/article.js`);
+//   const response = await graphql(`
+//     query {
+//     allContentfulArticle{
+//         edges {
+//           node {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   //Create service pages
 
-  response.data.allContentfulArticle.edges.map(({ node }) => (
-    createPage({
-      path: `/blog/${node.slug}`,
-      component: articleTemplate,
-      context: {
-        slug: node.slug,
-      },
-    })
-  ))
-}
+//   response.data.allContentfulArticle.edges.map(({ node }) => (
+//     createPage({
+//       path: `/blog/${node.slug}`,
+//       component: articleTemplate,
+//       context: {
+//         slug: node.slug,
+//       },
+//     })
+//   ))
+// }
 
 
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  if (stage.startsWith("develop")) {
-    actions.setWebpackConfig({
-      resolve: {
-        alias: {
-          "react-dom": "@hot-loader/react-dom",
-        },
-      },
-    })
-  }
-}
+// exports.onCreateWebpackConfig = ({ stage, actions }) => {
+//   if (stage.startsWith("develop")) {
+//     actions.setWebpackConfig({
+//       resolve: {
+//         alias: {
+//           "react-dom": "@hot-loader/react-dom",
+//         },
+//       },
+//     })
+//   }
+// }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Router } from '@reach/router';
 import Home from './home';
 import About from './about';
 import Service from './service';
 import Contact from './contact';
 import Project from './projects';
-import Blog from './blog';
+// import Blog from './blog';
 import SEO from '../components/SEO';
-import Modal from '../components/Modal';
+
 
 
 
 const Index = () => {
-    const [showModal, setShowModal] = useState(false);
 
-
-    const closeModal = () => {
-        setShowModal(false);
-    };
     return (
 
         <>
-            <SEO title="Home" description="This is my home page" />
+            <SEO title="Home" description="we build  Static website,CMS,Backend, Payment portal, Graphics,Database integration with your existed system , Ui development,  redesign old sites" />
 
 
-            <Modal showModal={showModal} closeModal={closeModal} />
+
 
 
             <Router>
-                <Home path="/" showModal={showModal} setShowModal={setShowModal} />
+                <Home path="/" />
 
                 <About path="/about" />
                 <Service path="/service" />
                 <Project path="/projects" />
-                <Contact path="/contact" showModal={showModal} setShowModal={setShowModal} />
-                <Blog path="/blog" />
+                <Contact path="/contact" />
+                {/* <Blog path="/blog" /> */}
             </Router>
 
 
