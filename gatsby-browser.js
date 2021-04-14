f6c46b22351cc1ddbcdbff85ee2e61a266a87cf5
Sj Bose <subhajeet.doc@gmail.com>
2021-04-14 21:09:39 +0530

    gatsby-ssr & browser require added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import Layout from './src/components/Layout';
-
+// import React from 'react';
+// import Layout from './src/components/Layout';
+const React = require('react');
+const Layout = require('./src/components/layout.js');
 
 
 export function wrapPageElement({ element, props }) {
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import Layout from './src/components/Layout';
+const React = require('react');
+const Layout = require('./src/components/layout.js');
+
 export function wrapPageElement({ element, props }) {
     return (
         <Layout {...props}>
